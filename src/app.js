a873c0051769b29a0276775eb9d2a1f68e7902af
Tim Bond <cookieguru@gmail.com>
2018-10-28 11:21:03 -0700

    Remove drawer image

--- src/app.js
+++ src/app.js
@@ -13,12 +13,6 @@ let navigationView = new tabris.NavigationView({
 
 tabris.ui.drawer.enabled = true;
 
-new tabris.ImageView({
-	left: 0, right: 0, top: 0, height: screen.width / 3,
-	image: 'images/drawer.jpg',
-	scaleMode: 'fill',
-}).appendTo(tabris.ui.drawer);
-
 let pageNames = [
 	'Stations',
 	'SFD Twitter',
@@ -26,13 +20,13 @@ let pageNames = [
 ];
 
 new tabris.CollectionView({
-	left: 0, top: 'prev()', right: 0, bottom: 0,
+	left: 0, top: 0, right: 0, bottom: 0,
 	itemCount: pageNames.length,
 	createCell: function createCell() {
 		let cell = new BorderedCell();
 		new tabris.TextView({
 			left: 17, centerY: 0,
-			font: '14px Roboto Medium',
+			font: '16px Roboto Medium',
 			textColor: '#212121',
 		}).appendTo(cell);
 		return cell;
