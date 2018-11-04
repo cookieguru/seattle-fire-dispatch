e85855258f12d4f22450e11c3c98e68f5d106b56
Tim Bond <cookieguru@gmail.com>
2018-11-04 11:30:46 -0800

    Fix build hook to remove location permission

--- cordova/remove_location_permission.js
+++ cordova/remove_location_permission.js
@@ -2,14 +2,10 @@
 const fs = require('fs');
 
 if(fs.existsSync('platforms/android')) {
-	let lines = fs.readFileSync('platforms/android/AndroidManifest.xml', 'utf8').split('\n');
 	const MANIFEST = 'platforms/android/AndroidManifest.xml';
+	let lines = fs.readFileSync(MANIFEST, 'utf8');
 
-	lines.forEach((line, index, object) => {
-		if(line.match(/ACCESS_COARSE_LOCATION|ACCESS_FINE_LOCATION/)) {
-			object.splice(index, 1);
-		}
-	});
+	lines = lines.replace(/\s+<uses-permission android:name="android\.permission\.ACCESS_(?:FINE|COARSE)_LOCATION" \/>/g, '');
 
-	fs.writeFileSync(MANIFEST, lines.join('\n'));
+	fs.writeFileSync(MANIFEST, lines);
 }
